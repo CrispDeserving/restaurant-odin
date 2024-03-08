4f225a540f9896a639df948bd911305e0ad4add7
Jabez Villamarzo <simplesimmering@gmail.com>
2024-03-08 15:52:12 +0800

    chore(install-package): html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 import { resolve } from 'path';
+import HTMLWebpackPlugin from 'html-webpack-plugin';
 
 export const entry = './src/index.js';
 export const output = {
     filename: 'main.js',
     path: resolve(__dirname, 'dist'),
+	plugins: [
+		new HTMLWebpackPlugin({
+			template: "src/index.html",
+		}),
+	],
 };
