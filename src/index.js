0660576f7866725ba2a5e28e0cff7ca78cf2a8ee
Jabez Villamarzo <simplesimmering@gmail.com>
2024-03-14 10:27:10 +0800

    Add bind_buttons function

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 import { Home } from "./Home/render.js";
+import { Menu } from "./Menu/render.js";
+import { replace_inside } from "./dsl-utils.js"; 
 
-function handle_home_click() {
-	document.querySelector('#content').appendChild(Home);
+const Content = document.querySelector('#content');
+
+function bind_buttons({ home_btn, menu_btn, about_btn }) {
+    home_btn.addEventListener("click", () => {
+	replace_inside(Content, Home);
+    });
+
+    menu_btn.addEventListener("click", () => {
+	replace_inside(Content, Menu);
+    });
+
+    about_btn.addEventListener("click", () => {
+	alert("todo!!");
+	// replace_inside(Content, About);
+    });
 }
 
 window.addEventListener('DOMContentLoaded', () => {
     console.log("hello world");
 
-    handle_home_click();
+    bind_buttons({
+	home_btn: document.querySelector('#home_btn'),
+	menu_btn: document.querySelector('#menu_btn'),
+	about_btn: document.querySelector('#about_btn'),
+    });
+    Content.appendChild(Home);
 });
