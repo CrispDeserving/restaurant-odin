f8d1d8e20d3df45491e510eb1fb05ffe92ec7c36
Jabez Villamarzo <simplesimmering@gmail.com>
2024-03-15 00:02:05 +0800

    Add About and AboutHeader elements

---
+++ src/About/render.js
@@ -0,0 +1,78 @@
+import { children } from "../dsl-utils";
+import './style.css';
+
+function init_about_element() {
+	const dummy_image_element = "replace-me";
+	const About = children(document.createElement('div'),
+		document.createElement('h2'),
+
+		children(document.createElement('ul'),
+			[
+				"Twitter",
+				"Contact Number",
+				"Location",
+			].map((detail_name,	i) => {
+				const Detail = children(document.createElement('li'),
+					document.createElement(dummy_image_element),
+
+					children(document.createElement('div'),
+						document.createElement('p'),
+						document.createElement('p'),
+					),
+				);
+
+				Detail.classList.add('Detail');
+				Detail.setAttribute('data-index', i);
+
+				Detail.querySelector('p:nth-child(1)').classList.add('DetailName');
+				Detail.querySelector('p:nth-child(1)').innerText = `${detail_name}: `;
+
+				Detail.querySelector('p:nth-child(2)').classList.add('Details');
+
+				return Detail;
+			}),
+		),
+	);
+
+	About.classList.add('About');
+	About.querySelector('h2').innerText = "Restaurant Odin";
+	
+	{
+		const temp = About.querySelector(`.Detail[data-index="0"]`);
+		temp.querySelector('.Details').innerText = "@resto-odin-unofficial";
+	}
+	{
+		const temp = About.querySelector(`.Detail[data-index="1"]`);
+		temp.querySelector('.Details').innerText = "(+00)973 786 6346 (09-RESTO-ODIN)";
+	}
+	{
+		const temp = About.querySelector(`.Detail[data-index="2"]`);
+		temp.querySelector('.Details').innerText = "Olympus, Land of the Demigods";
+	}
+
+	return About;
+}
+
+function init_about_footer_element() {
+	const AboutFooter = children(document.createElement('footer'),
+		document.createElement('h2'),
+		document.createElement('p'),
+		document.createElement('p'),
+	);
+	AboutFooter.classList.add('AboutFooter');
+
+	AboutFooter.querySelector('h2').innerText = "Restaurant Odin";
+	AboutFooter.querySelector(':nth-child(1 of p)').innerText = "We're not Hiring...";
+	AboutFooter.querySelector(':nth-child(2 of p)').innerText = "But come join us!";
+
+	return AboutFooter;
+}
+
+
+export const About = init_about_element();
+export const AboutFooter = init_about_footer_element();
+
+export default {
+	About,
+	AboutFooter,
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { Home } from "./Home/render.js";
 import { Menu } from "./Menu/render.js";
 import { Footer } from "./Footer/render.js";
+import { About, AboutFooter } from "./About/render.js";
 import { replace_inside } from "./dsl-utils.js"; 
 
 const Content = document.querySelector('#content');
@@ -8,15 +9,26 @@ const Content = document.querySelector('#content');
 function bind_dom_elements({ home_btn, menu_btn, about_btn, dummy_footer }) {
     home_btn.addEventListener("click", () => {
 	replace_inside(Content, Home);
+
+	if (dummy_footer.querySelector('.Footer') === null) {
+	    replace_inside(dummy_footer, Footer);
+	}
     });
 
     menu_btn.addEventListener("click", () => {
 	replace_inside(Content, Menu);
+
+	if (dummy_footer.querySelector('.Footer') === null) {
+	    replace_inside(dummy_footer, Footer);
+	}
     });
 
     about_btn.addEventListener("click", () => {
-	alert("todo!!");
-	// replace_inside(Content, About);
+	replace_inside(Content, About);
+
+	if (dummy_footer.querySelector('.Footer') !== null) {
+	    replace_inside(dummy_footer, AboutFooter);
+	}
     });
 
     replace_inside(dummy_footer, Footer);
