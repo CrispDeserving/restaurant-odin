37d1a374375ef89dc41fea9ecccfb5f45073e9cb
Jabez Villamarzo <simplesimmering@gmail.com>
2024-03-16 21:56:33 +0800

    Add icon assets to Footer
    Also rearrange the DOM elements on .Detail

--- src/Footer/render.js
+++ src/Footer/render.js
@@ -1,4 +1,7 @@
-import { children } from "../dsl-utils";
+import { children, replace_node } from "../dsl-utils";
+import MapPin from '../assets/map-pin.svg';
+import Phone from '../assets/phone.svg';
+import Twitter from '../assets/twitter.svg';
 import './style.css';
 
 function init_footer_element() {
@@ -16,21 +19,21 @@ function init_footer_element() {
 				"Location",
 			].map((detail_name,	i) => {
 				const Detail = children(document.createElement('li'),
-					document.createElement(dummy_image_element),
-
 					children(document.createElement('div'),
-						document.createElement('p'),
-						document.createElement('p'),
+						document.createElement(dummy_image_element),
+						document.createElement('span'),
 					),
+
+					document.createElement('p'),
 				);
 
 				Detail.classList.add('Detail');
 				Detail.setAttribute('data-index', i);
 
-				Detail.querySelector('p:nth-child(1)').classList.add('DetailName');
-				Detail.querySelector('p:nth-child(1)').innerText = `${detail_name}: `;
+				Detail.querySelector('div > span').classList.add('DetailName');
+				Detail.querySelector('div > span').innerText = `${detail_name}: `;
 
-				Detail.querySelector('p:nth-child(2)').classList.add('Details');
+				Detail.querySelector('p').classList.add('Details');
 
 				return Detail;
 			}),
@@ -43,14 +46,35 @@ function init_footer_element() {
 	{
 		const temp = Footer.querySelector(`.Detail[data-index="0"]`);
 		temp.querySelector('.Details').innerText = "@resto-odin-unofficial";
+
+		const image = new Image();
+		image.setAttribute('src', Twitter);
+		image.setAttribute('alt', "A brand icon from the website formerly called Twitter.");
+
+		const dummy_element = temp.querySelector(`${dummy_image_element}`);
+		replace_node(dummy_element, image);
 	}
 	{
 		const temp = Footer.querySelector(`.Detail[data-index="1"]`);
 		temp.querySelector('.Details').innerText = "(+00)973 786 6346 (09-RESTO-ODIN)";
+
+		const image = new Image();
+		image.setAttribute('src', Phone);
+		image.setAttribute('alt', "A phone icon.");
+
+		const dummy_element = temp.querySelector(`${dummy_image_element}`);
+		replace_node(dummy_element, image);
 	}
 	{
 		const temp = Footer.querySelector(`.Detail[data-index="2"]`);
 		temp.querySelector('.Details').innerText = "Olympus, Land of the Demigods";
+
+		const image = new Image();
+		image.setAttribute('src', MapPin);
+		image.setAttribute('alt', "An icon signifying a pinned location in a map.");
+
+		const dummy_element = temp.querySelector(`${dummy_image_element}`);
+		replace_node(dummy_element, image);
 	}
 
 	return Footer;
