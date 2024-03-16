41e50767ac6892c7c7e456cd82d54d931e566f83
Jabez Villamarzo <simplesimmering@gmail.com>
2024-03-16 22:04:10 +0800

    Add icon assets to About

--- src/About/render.js
+++ src/About/render.js
@@ -1,4 +1,7 @@
-import { children } from "../dsl-utils";
+import { children, replace_node } from "../dsl-utils";
+import MapPin from '../assets/map-pin.svg';
+import Phone from '../assets/phone.svg';
+import Twitter from '../assets/twitter.svg';
 import './style.css';
 
 function init_about_element() {
@@ -40,14 +43,35 @@ function init_about_element() {
 	{
 		const temp = About.querySelector(`.Detail[data-index="0"]`);
 		temp.querySelector('.Details').innerText = "@resto-odin-unofficial";
+
+		const image = new Image();
+		image.setAttribute('src', Twitter);
+		image.setAttribute('alt', "A brand icon from the website formerly called Twitter.");
+
+		const dummy_element = temp.querySelector(`${dummy_image_element}`);
+		replace_node(dummy_element, image);
 	}
 	{
 		const temp = About.querySelector(`.Detail[data-index="1"]`);
 		temp.querySelector('.Details').innerText = "(+00)973 786 6346 (09-RESTO-ODIN)";
+
+		const image = new Image();
+		image.setAttribute('src', Phone);
+		image.setAttribute('alt', "A phone icon.");
+
+		const dummy_element = temp.querySelector(`${dummy_image_element}`);
+		replace_node(dummy_element, image);
 	}
 	{
 		const temp = About.querySelector(`.Detail[data-index="2"]`);
 		temp.querySelector('.Details').innerText = "Olympus, Land of the Demigods";
+		
+		const image = new Image();
+		image.setAttribute('src', MapPin);
+		image.setAttribute('alt', "An icon signifying a pinned location in a map.");
+
+		const dummy_element = temp.querySelector(`${dummy_image_element}`);
+		replace_node(dummy_element, image);
 	}
 
 	return About;
