1f8ccf218916cf4d9fb99fef12b450733e520976
Jabez Villamarzo <simplesimmering@gmail.com>
2024-03-20 21:31:19 +0800

    Add webpack-merge as dependency, update scripts

--- webpack.common.js
+++ webpack.common.js
@@ -3,14 +3,9 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
-	mode: 'development',
 	output: {
 		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
-	},
-	devtool: 'inline-source-map',
-	devServer: {
-		static: './dist',
 	},
 	module: {
 		rules: [
